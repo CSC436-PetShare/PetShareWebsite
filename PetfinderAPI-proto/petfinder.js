47310ef921ddb734a29e022e3be47bbf8320f2e4
EujinKo <42593556+EujinKo@users.noreply.github.com>
2020-10-09 15:00:34 -0700

    Testing with getting pets with key

--- PetfinderAPI-proto/petfinder.js
+++ PetfinderAPI-proto/petfinder.js
@@ -1,14 +1,37 @@
 const petFinderKey = "S2t3nrRa8vSmzQDxActpsXAhglEQdF5rvVQWfLBKQlT3ByXXia";
 const petFinderSecret = "dyFrO08uwicmha6hFDziKndaMhjgk4Wk1joLrYgd";
 
-var petfinder = require("@petfinder/petfinder-js");
-var client = new petfinder.Client({apiKey: petFinderKey, secret: petFinderSecret});
+var pet_spec = {
+    type: "",
+    breed: "",
+    limit: 50,
+}
 
-client.animal.search()
-    .then(function (response) {
-        // Do something with `response.data.animals`
-        console.log(response.data);
+var petfinder = require("@petfinder/petfinder-js");
+
+
+//Setup type of animal you want to search before calling the find_pet function
+// to return the results of available pets
+//Available settings: type, breed
+
+var settings = function (spec, value) {
+    pet_spec[spec] = value;
+}
+settings("type", "Dog");
+console.log(pet_spec);
+var client = new petfinder.Client({ apiKey: petFinderKey, secret: petFinderSecret });;
+
+var find_pet = function () {
+    client.animal.search({
+
+        type: "Dog",
     })
-    .catch(function (error) {
-        // Handle the error
-    });
+        .then(function (response) {
+            // Do something with `response.data.animals`
+            console.log(response.data);
+        })
+        .catch(function (error) {
+            // Handle the error
+        });
+}
+find_pet();
