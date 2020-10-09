7d6c659fdd4b693d1f38b1c6a9331112c2d48372
EujinKo <42593556+EujinKo@users.noreply.github.com>
2020-10-09 14:10:34 -0700

    Error trying to get authenticated from Petfinder API

---
+++ PetfinderAPI-proto/petfinder.js
@@ -0,0 +1,15 @@
+const petFinderKey = "S2t3nrRa8vSmzQDxActpsXAhglEQdF5rvVQWfLBKQlT3ByXXia";
+const petFinderSecret = "dyFrO08uwicmha6hFDziKndaMhjgk4Wk1joLrYgd";
+
+const token_url = '<https://api.petfinder.com/v2/oauth2/token>';
+const token_body = 'grant_type=client_credentials&client_id={petFinderKey}&client_secret={petFinderSecret}';
+
+const access_token = {
+    "access_token": token_body,
+    "token_type": "Bearer",
+    "expires_in": 3600,
+    "access_token": "..."
+}
+
+var request = new XMLHttpRequest("GET","Authorization: Bearer {access_token}");
+console.log(request.responseURL);
