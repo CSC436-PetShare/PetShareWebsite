449f570f09b7ae337a5653fb8d491e2d2d1acf87
EujinKo <42593556+EujinKo@users.noreply.github.com>
2020-10-16 14:40:19 -0700

    Needs to test on live module page

--- PetfinderAPI-proto/petfinder.js
+++ PetfinderAPI-proto/petfinder.js
@@ -1,3 +1,8 @@
+
+import { Client } from "/@petfinder/petfinder-js";
+// var petfinder = require("@petfinder/petfinder-js");
+
+
 const petFinderKey = "S2t3nrRa8vSmzQDxActpsXAhglEQdF5rvVQWfLBKQlT3ByXXia";
 const petFinderSecret = "dyFrO08uwicmha6hFDziKndaMhjgk4Wk1joLrYgd";
 
@@ -6,7 +11,6 @@ var pet_spec = {
     //MAX LIMIT: 50
 }
 
-var petfinder = require("@petfinder/petfinder-js");
 
 
 //Setup type of animal you want to search before calling the find_pet function
@@ -18,7 +22,7 @@ var settings = function (spec, value) {
 }
 
 var find_pet = function () {
-    var client = new petfinder.Client({
+    var client = new Client({
         apiKey: petFinderKey,
         secret: petFinderSecret 
     });;
@@ -35,7 +39,7 @@ var find_pet = function () {
 }
 //Returns information about the type
 var fillViewWithPetType = function(){
-    var client = new petfinder.Client({
+    var client = new Client({
         apiKey: petFinderKey,
         secret: petFinderSecret 
     });;
@@ -44,17 +48,17 @@ var fillViewWithPetType = function(){
         response => {
             var types = response.data.types;
 
-            // var html_select = document.createElement("SELECT");
-            // html_select.setAttribute("id","typeSelect");
-            // view.appendChild(html_select);
-            // view.appendChild(html_input);
+            var html_select = document.createElement("Select");
+            html_select.setAttribute("id","typeSelect");
+            view.appendChild(html_select);
+            view.appendChild(html_input);
             for(i in types){
-                // var option = document.createElement("option");
-                // option.setAttribute("value", types[i].name);
-                // html_select.appendChild(option);
+                var option = document.createElement("option");
+                option.setAttribute("value", types[i].name);
+                html_select.appendChild(option);
                 console.log( types[i].name);
             }
-            // console.log(html_select.innerText);
+            console.log(html_select.innerText);
         }
     )
 
@@ -63,4 +67,3 @@ var fillViewWithPetType = function(){
 // settings("type", "Dog");
 // settings("coat", "Long");
 // settings("size","Extra Large");
-fillViewWithPetType();
