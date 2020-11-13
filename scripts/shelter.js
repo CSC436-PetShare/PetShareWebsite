783437454a0219ddbbbaf00f346591eaf9313368
EujinKo <42593556+EujinKo@users.noreply.github.com>
2020-11-13 11:45:36 -0700

    Update shelter.js

--- scripts/shelter.js
+++ scripts/shelter.js
@@ -29,23 +29,59 @@ var setDropdown = function(dropdownEle, array){
 
 }
 
+//This function renders each element of the array
+var _render = function(search_result){
+	var table = document.getElementById('shelterTable');
+	var name = search_result['name'];
+	var url = search_result['url'];
+
+	var row = document.createElement("TR");
+	table.appendChild(row);
+
+	var t_name_data = document.createElement("TD");
+	var t_name_data_text = document.createTextNode(name);
+	t_name_data.appendChild(t_name_data_text);
+
+	var t_url_data = document.createElement("TD");
+
+	var a = document.createElement("a");
+	a.href = url;
+	var t_url_data_text = document.createTextNode("Click to Visit");
+	a.appendChild(t_url_data_text);
+	t_url_data.appendChild(a);
+
+	row.appendChild(t_url_data);
+	row.appendChild(t_name_data);
+}
+
+
+var set_up_table = function(){
+	var view = document.getElementById('shelterView');
+	view.innerHTML = "";
+	var table = document.createElement("TABLE");
+	table.setAttribute("id", "shelterTable");
+	view.appendChild(table);
+}
+
 window.addEventListener('DOMContentLoaded', function() {
 	var locationDropdown = document.getElementById('location');
 	petfinder_controller.returnAvailableStates().then(arr=>{
+		console.log(arr);
 		locations = arr;
 		setDropdown(locationDropdown, locations);
     })
     
 
     var locationValue = "";
-    var view = document.getElementById('shelterView');
+    
 	locationDropdown.addEventListener("change", function(){
         locationValue = this.value;
-        view.innerHTML = "";
+        
         petfinder_controller.searchOrganization(locationValue).then(
             organizations =>{
+				set_up_table();
                 organizations.forEach(function(element){
-                    view.innerHTML += element['name']+'<br>';
+					_render(element);
                 });
             }
         );
